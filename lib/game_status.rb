b2fe489f5ad8067310b3482b0b803596b2ab0a1f
Cameron Bisca <cjbisca@gmail.com>
2020-10-11 01:10:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,9 +9,9 @@ WIN_COMBINATIONS = [
   [0, 1, 2], #toprow 
   [3, 4, 5], #middlerow
   [6, 7, 8], #bottomrow
-  [0, 6, 3], #leftcolumn
-  [1, 4, 6], #middlecolumn
+  [0, 3, 6], #leftcolumn
+  [1, 4, 7], #middlecolumn
   [2, 5, 8], #rightcolumn
   [0, 4, 8], #1diagonal 
-  [2, 4, 8] #2diagonal 
+  [2, 4, 6] #2diagonal 
   ]
