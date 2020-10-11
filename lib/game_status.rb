1fc0d1be4e4458013f8ad0a2279506622aca864f
Cameron Bisca <cjbisca@gmail.com>
2020-10-11 01:05:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,7 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+# Define your WIN_COMBINATIONS constan
+def won?
+
+WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
